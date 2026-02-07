77f1bdc65f72d7eee502e67321d23e9fc0103414
asma <asmamhamdi@gmail.com>
2026-02-07 08:01:54 +0100

    Initialisation du projet React - gestion des chantiers

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gestion des chantiers 🏗️</h1>
+      <p>Projet React avec structure propre</p>
+    </div>
+  );
+}
+
+export default App;
