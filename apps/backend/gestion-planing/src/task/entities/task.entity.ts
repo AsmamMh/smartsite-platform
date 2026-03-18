6cc61d0d227b4b233e78efc205dc3f56b48542e6
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-03-18 01:42:19 +0100

    feat: implement task management module with CRUD operations

---
+++ apps/backend/gestion-planing/src/task/entities/task.entity.ts
@@ -0,0 +1,45 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Task extends Document {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ type: Types.ObjectId , ref: 'Milestone',required:true })
+  milestoneId: Types.ObjectId;
+
+  
+  @Prop({ type: [String], default: [] })
+  assignedUsers: string;
+
+  @Prop()
+  priority: string;
+
+  @Prop()
+  projectId: string;
+
+  @Prop()
+  siteId: string;
+  @Prop()
+  createdBy: string;
+
+  @Prop()
+  updatedBy: string;
+
+  @Prop({ default: 'TODO' })
+  status: string;
+
+  @Prop({ default: 0 })
+  progress: number;
+
+  @Prop()
+  startDate: Date;
+
+  @Prop()
+  endDate: Date;
+}
+export const TaskSchema = SchemaFactory.createForClass(Task);
